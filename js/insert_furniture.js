2ed722904f18a36fbb18a62b332fa6bd8afa4723
eom012 <33171438+eom012@users.noreply.github.com>
2019-11-22 13:34:17 +0900

    ..

--- js/insert_furniture.js
+++ js/insert_furniture.js
@@ -2,50 +2,56 @@ var gl;
 
 window.onload = function () {
 	let scene = new THREE.Scene();
-	let light;
-	let camera;
+	let light, gird, gridH, camera;
+	let rayCast, mouse;	
 	let loader; // OBJLoader 객체를 넣을 변수를 선언
+	let cl;
+	// testing~ start
+	// mouse click event function
+	let onMouseClick = function(e){
+		let gap1 = e.clientX - e.offsetX;
+		let gap2 = e.clientY - e.offsetY;
+		mouse.x = ((e.clientX - gap1) / (window.innerWidth*0.375))*2 -1;
+		mouse.y = -((e.clientY - gap2) / (window.innerHeight*0.375))*2 +1;
+		rayCast.setFromCamera(mouse,camera);
+		loadObjLoader(rayCast.ray.at(50), './furniture/bathroomCabinet.obj');
+	}
+	// testing~ end
 	
 	initThree();
 	addDirectionalLight();
-	
+	addGridView();
 
-/** *************Insert Furniture Mode********************** */
+	// button event
+	/** *************Insert Furniture Mode********************** */
     var InsertButton = document.getElementById("insert");
-	InsertButton.addEventListener("click", function(event) { 
+	InsertButton.addEventListener("click", function(event) {
+		camera.position.x = 0;
+		camera.position.y = 6;
+		camera.position.z = 0;
         cl=1;
 	});
-
+	
+	/** *************Change View Space Mode********************** */
+    var ViewButton = document.getElementById("view");
+	ViewButton.addEventListener("click", function(event) { 
+        cl=0;
+	});
+	/*
+	// click event for inserting furniture
     window.addEventListener("click", function(event){
 		if(cl==1&&event.clientY>50){	
         // check log
 		console.log('clcik event listener');
 		console.log(event.clientX, event.clientY);
 		// load OBJ file
-		loadObjLoader(event.clientX, event.clientY, './furniture/bathroomCabinet.obj');
+		loadObjLoader(position, './furniture/bathroomCabinet.obj');
 		// console.log(loader.position);
         }
     } );
-
-
-/** *************Change View Space Mode********************** */
-    var ViewButton = document.getElementById("view");
-	    ViewButton.addEventListener("click", function(event) { 
-        cl=0;
-        
-	});
-
-
-
-	// loadObjLoader('./furniture/cat.obj');
-
-	/**
-	 * DirectionalLight를 추가하는 함수
-	 * 
-	 * @method addDirectionalLight
-	 */
+*/
+	/** DirectionalLight를 추가하는 함수 */
 	function addDirectionalLight() {
-
 		light = new THREE.DirectionalLight(0xffffff, 1);
 		light.castShadow = true;
 		light.position.x = 5;
@@ -53,19 +59,30 @@ window.onload = function () {
 		light.position.z = 5;
 		scene.add(light);
 	}
+	
+	/** GridHelper를 추가하는 함수 */
+	function addGridView() {
+		// add grid
+		var size = 10;
+		var divisions = 100;
+		grid = new THREE.Object3D();
+		gridH = new THREE.GridHelper(size, divisions, 0x0000ff, 0x808080);
+		gridH.position.y = 0;
+		gridH.position.x = 0;
+		gridH.position.z = 0;
+		gridH.rotation.x = 0;
+		grid.add(gridH);
+		scene.add(grid);
+	}
 
-	/**
-	 * .obj 파일의 모델을 로드하는 함수
-	 * 
-	 * @method loadObjLoader
-	 */
-	function loadObjLoader(x, y, obj) {
+	/** .obj 파일의 모델을 로드하는 함수 */
+	function loadObjLoader(position, obj) {
 		loader = new THREE.OBJLoader();
 		loader.load(obj, function (object) {
-			// finish to load model
+			// set position of object
+			object.position.set(position.x, position.y, position.z);
+			// add object
 			scene.add(object);
-			// set position
-			object.position.set((x-366)/50, 0, (y-477)/50);
 		}, function (xhr) {
 			// loading model
 			console.log(xhr.loaded / xhr.total * 100, '% loaded');
@@ -75,31 +92,35 @@ window.onload = function () {
 		});
 	}
 
-	/**
-	 * Threejs 초기화 함수
-	 * 
-	 * @method initThree
-	 */
-	 
+	/** Threejs 초기화 함수 */
 	function initThree() {
-		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+		// set camera position
+		camera.position.x = 0;
+		camera.position.y = 6;
+		camera.position.z = 0;
+		
+		//testing~ start
+		rayCast = new THREE.Raycaster();
+		mouse = new THREE.Vector2();
+		mouse.x = mouse.y = -1;
+		
+		//testing~ end
+		
 		let renderer = new THREE.WebGLRenderer({
 			antialias: true
 		});
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.shadowMap.enabled = true;
 		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		renderer.domElement.addEventListener('click', onMouseClick, false);
 		document.body.appendChild(renderer.domElement);
 
+		// x,y,z lines
 		/*
 		 * let axes = new THREE.AxesHelper(5); scene.add(axes);
 		 */
-
-		camera.position.x = 2;
-		camera.position.y = 1;
-		camera.position.z = 1;
-
+		
 		controls = new THREE.OrbitControls(camera, renderer.domElement);
 		controls.rotateSpeed = 1.0;
 		controls.zoomSpeed = 1.2;
@@ -107,22 +128,16 @@ window.onload = function () {
 		controls.minDistance = -50;
 		controls.maxDistance = 10;
 		
-		grid = new THREE.Object3D();
-		  
-		var size = 8;
-		var divisions = 100;
-		
-		var gridH = new THREE.GridHelper(size, divisions, 0x0000ff, 0x808080);
-		gridH.position.y = 0;
-		gridH.position.x = 0;
-		gridH.rotation.x = 0;
-		grid.add(gridH);
-		
-		scene.add(grid);
-
 		function animate() {
 			requestAnimationFrame(animate);
-
+			if (cl == 1) {
+				// console.log('mode of inserting furniture!')
+				camera.position.x = 0;
+				camera.position.y = 6;
+				camera.position.z = 0;
+			}
+			else {
+			}
 			renderer.render(scene, camera);
 			controls.update();
 		}
